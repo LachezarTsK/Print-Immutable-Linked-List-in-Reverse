7e966829deba6af65fee63a433de22a1736d1c8c
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-12-09 11:54:48 +0100

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -10,10 +10,16 @@ class Solution {
 }
 
 /*
- This is the ImmutableListNode's API interface. It is created by leetcode.com 
- and the problem must be solved only by applying this interface.
+This is the ImmutableListNode's API interface.
+
+The interface is implemented internally by leetcode.com and should not be implemented
+by the person solving the problem. However, the problem must be solved only by applying this interface.
+
+If the participant tries to implement it on the website or solve the problem without applying the interface,
+the test cases on leetcode.com will not be successful.
  */
 interface ImmutableListNode {
+
     public void printValue(); // print the value of this node.
     public ImmutableListNode getNext(); // return the next node.
 };
